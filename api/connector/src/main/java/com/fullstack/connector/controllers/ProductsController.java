40bc19c5d8b9300741d4e4e090fbe87ff54399aa
jtstrader <11006561@live.mercer.edu>
2021-07-09 06:30:20 -0700

    controller defined with GET and POST

--- api/connector/src/main/java/com/fullstack/connector/controllers/ProductsController.java
+++ api/connector/src/main/java/com/fullstack/connector/controllers/ProductsController.java
@@ -1,10 +1,30 @@
 package com.fullstack.connector.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fullstack.connector.models.Product;
+import com.fullstack.connector.repositories.ProductsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductsController {
-    
+    @Autowired
+    private ProductsRepository productsRepository;
+
+    @GetMapping // full list
+    public List<Product> list() {
+        return productsRepository.findAll();
+    }
+
+    @GetMapping("{id}") // specific product
+    public Product get(@PathVariable Long id) {
+        return productsRepository.getById(id);
+    }
+
+    @PostMapping // create new product
+    public Product create(@RequestBody final Product product) {
+        return productsRepository.saveAndFlush(product);
+    }
 }
