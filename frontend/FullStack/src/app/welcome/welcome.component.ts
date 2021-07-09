9109811392fc365406b8ca089e82fbd107f96b42
jtstrader <11006561@live.mercer.edu>
2021-07-09 10:38:27 -0700

    welcome page started

--- frontend/FullStack/src/app/welcome/welcome.component.ts
+++ frontend/FullStack/src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MAT_RIPPLE_GLOBAL_OPTIONS } from '@angular/material/core';
 
 @Component({
   selector: 'app-welcome',
@@ -7,7 +8,110 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
+  multi = [
+    {
+      "name": "Germany",
+      "series": [
+        {
+          "name": "1990",
+          "value": 62000000
+        },
+        {
+          "name": "2010",
+          "value": 73000000
+        },
+        {
+          "name": "2011",
+          "value": 89400000
+        }
+      ]
+    },
+  
+    {
+      "name": "USA",
+      "series": [
+        {
+          "name": "1990",
+          "value": 250000000
+        },
+        {
+          "name": "2010",
+          "value": 309000000
+        },
+        {
+          "name": "2011",
+          "value": 311000000
+        }
+      ]
+    },
+  
+    {
+      "name": "France",
+      "series": [
+        {
+          "name": "1990",
+          "value": 58000000
+        },
+        {
+          "name": "2010",
+          "value": 50000020
+        },
+        {
+          "name": "2011",
+          "value": 58000000
+        }
+      ]
+    },
+    {
+      "name": "UK",
+      "series": [
+        {
+          "name": "1990",
+          "value": 57000000
+        },
+        {
+          "name": "2010",
+          "value": 62000000
+        }
+      ]
+    }
+  ];
+  view: [number, number] = [700, 300];
+
+  // options
+  legend: boolean = true;
+  showLabels: boolean = true;
+  animations: boolean = true;
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = 'Year';
+  yAxisLabel: string = 'Population';
+  timeline: boolean = true;
+
+  colorScheme = {
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+  };
+
   constructor() { }
+
+  // failure rate gauge (ACME)
+  acme_gaugeValue = 72;
+  acme_gaugeLabel = "ACME Failure Rate";
+  acme_gaugeAppendText = "%";
+  acme_gaugeThickness = 8;
+  acme_color = 'green';
+
+  // failure rate gauge (HoleBoat)
+  hb_gaugeValue = 94;
+  hb_gaugeLabel = "HoleBoat Failure Rate";
+  hb_gaugeAppendText = "%";
+  hb_gaugeThickness = 8;
+  hb_color = 'red';
+
+  // line chart data
+  
 
   ngOnInit(): void {
   }
