aa0f4d507006e44fc8b98fb1f4d402a758745ed8
jtstrader <11006561@live.mercer.edu>
2021-07-09 11:02:00 -0700

    boilerplate

--- frontend/FullStack/src/app/interfaces/iproduct.ts
+++ frontend/FullStack/src/app/interfaces/iproduct.ts
@@ -1,2 +1,6 @@
 export interface IProduct {
+    product_id: number;
+    product_name: string;
+    amount_in_stock: number;
+    cost: number;
 }
--- frontend/FullStack/src/app/services/product.service.ts
+++ frontend/FullStack/src/app/services/product.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(http: HttpClient) { }
+
+  
+
 }
