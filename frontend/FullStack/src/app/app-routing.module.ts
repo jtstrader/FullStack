a75c89b25abae943438ffb518ac7bc1ecd88bef4
jtstrader <11006561@live.mercer.edu>
2021-07-09 08:15:26 -0700

    routing and topnav established

--- frontend/FullStack/src/app/app-routing.module.ts
+++ frontend/FullStack/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { InformationComponent } from './information/information.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: WelcomeComponent
+  },
+  {
+    path: 'welcome', component: WelcomeComponent
+  },
+  {
+    path: 'products', component: ProductListComponent
+  },
+  {
+    path: 'about', component: AboutComponent
+  },
+  {
+    path: 'pinfo', component: InformationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
