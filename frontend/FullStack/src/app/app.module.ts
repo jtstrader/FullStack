aaa6a14e12d4cdc81b4559fcf659269489345a43
jtstrader <11006561@live.mercer.edu>
2021-07-09 08:15:11 -0700

    boilerplate (angular material and base components)

--- frontend/FullStack/src/app/app.module.ts
+++ frontend/FullStack/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Angular Material Modules
+import { MatCard, MatCardModule } from '@angular/material/card';
+import { AboutComponent } from './about/about.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { InformationComponent } from './information/information.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProductListComponent,
+    WelcomeComponent,
+    InformationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
